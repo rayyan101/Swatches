435ea46fbe427341ce5fdb33a303448ec14aa69e
rayyan <rayyanjamil101@gmail.com>
2022-07-14 13:34:56 +0500

    Email Sender

--- includes/class-es-loader.php
+++ includes/class-es-loader.php
@@ -2,31 +2,31 @@
 	/**
 	 * This class is Loader Class we are using for Enqueue Scrips Style and including other Classes and Files
 	 *
-	 * @package Woocommerce Swatches
+	 * @package Email Sender
 	 */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-if ( ! class_exists( 'WS_Loader' ) ) {
+if ( ! class_exists( 'ES_Loader' ) ) {
 
 	/**
 	 * Class WS_Loader. ()
 	 */
-	class WS_Loader {
+	class ES_Loader {
 
 		/**
 		 * Constructor.
 		 */
 		public function __construct() {
 			$this->includes();
-			add_action( 'wp_enqueue_scripts', array( $this, 'WS_enqueue_scripts_client' ) );
+			add_action( 'wp_enqueue_scripts', array( $this, 'es_enqueue_scripts_client' ) );
 		}
 
 		/**
 		 * Function for Enqueue Scripts and Style on Client side
 		 */
-		public function ws_enqueue_scripts_client() {
+		public function es_enqueue_scripts_client() {
 			wp_enqueue_script( 'WS_client_js', plugin_dir_url( __DIR__ ) . '/assets/js/client.js', array( 'jquery' ), wp_rand() );
 			wp_localize_script( 'WS_client_js', 'ajax_object', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 			wp_enqueue_style( 'WS_plugin_style', plugin_dir_url( __DIR__ ) . '/assets/css/clientstyle.css', array(), '1.0' );
@@ -37,10 +37,10 @@ if ( ! class_exists( 'WS_Loader' ) ) {
 		 * Function for Including Files and Classes
 		 */
 		public function includes() {
-			include_once 'class-WS-SwitchesTab.php';
-			include_once 'class-WS-Variations.php';
+			include_once 'class-ES-Orderstatus.php';
+			include_once 'class-es-sendingemail.php';
 		}
 	}
 }
-new WS_Loader();
+new ES_Loader();
 
