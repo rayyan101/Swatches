30f6887f09b2cd8b58dc55a36f09340fc0783236
rayyan <rayyanjamil101@gmail.com>
2022-07-14 16:14:37 +0500

    Sniffer

--- includes/class-es-orderstatus.php
+++ includes/class-es-orderstatus.php
@@ -19,28 +19,32 @@ if ( ! class_exists( 'ES_Orderstatus' ) ) {
 		 *  Constructor.
 		 */
 		public function __construct() {
-			add_action( 'init', array( $this, 'register_shipping_order_status'), 20 );
-			add_filter( 'wc_order_statuses', array( $this,'shipping_order_status_editpage_dropdown'), 20, 1 );
-			add_filter( 'bulk_actions-edit-shop_order', array( $this,'shipping_dropdown_bulk_actions_shop_order'), 20, 1 );
+			add_action( 'init', array( $this, 'register_shipping_order_status' ), 20 );
+			add_filter( 'wc_order_statuses', array( $this, 'shipping_order_status_editpage_dropdown' ), 20, 1 );
+			add_filter( 'bulk_actions-edit-shop_order', array( $this, 'shipping_dropdown_bulk_actions_shop_order' ), 20, 1 );
 		}
-		
+
 		/**
 		 *  Registered Shipping Order Status.
 		 */
 		public function register_shipping_order_status() {
-			register_post_status( 'wc-shipping', array(
-				'label'                     => _x( 'Shipping', 'Order status', 'woocommerce' ),
-				'public'                    => true,
-				'exclude_from_search'       => false,
-				'show_in_admin_all_list'    => true,
-				'show_in_admin_status_list' => true,
-				'label_count'               => _n_noop( 'Shipping<span class="count">(%s)</span>', 'Shipping delivery <span class="count">(%s)</span>', 'woocommerce' )
-			) );
+			register_post_status(
+				'wc-shipping',
+				array(
+					'label'                     => _x( 'Shipping', 'Order status', 'woocommerce' ),
+					'public'                    => true,
+					'exclude_from_search'       => false,
+					'show_in_admin_all_list'    => true,
+					'show_in_admin_status_list' => true,
+				)
+			);
 		}
 
 
 		/**
-		 *  Adding shipping status to order edit pages dropdown.
+		 * Adding shipping status to order edit pages dropdown.
+		 *
+		 * @param array $order_statuses all order status.
 		 */
 		public function shipping_order_status_editpage_dropdown( $order_statuses ) {
 			$order_statuses['wc-shipping'] = _x( 'Shipping', 'Order status', 'woocommerce' );
@@ -48,7 +52,9 @@ if ( ! class_exists( 'ES_Orderstatus' ) ) {
 		}
 
 		/**
-		 *  Adding Shipping status to admin order list bulk dropdown.
+		 * Adding Shipping status to admin order list bulk dropdown.
+		 *
+		 * @param array $actions action.
 		 */
 		public function shipping_dropdown_bulk_actions_shop_order( $actions ) {
 			$actions['mark_shipping'] = __( 'Mark Shipping', 'woocommerce' );
@@ -57,4 +63,4 @@ if ( ! class_exists( 'ES_Orderstatus' ) ) {
 	}
 }
 new ES_Orderstatus();
-?>
+
--- includes/class-es-sendingemail.php
+++ includes/class-es-sendingemail.php
@@ -18,52 +18,53 @@ if ( ! class_exists( 'ES_Sendingemail' ) ) {
 		/**
 		 *  Constructor.
 		 */
-		public function __construct() { 
-			add_action('woocommerce_order_status_changed', array( $this, 'sending_email_on_shipping'), 10, 4);
-			add_action('woocommerce_email_order_details', array( $this, 'email_customizer') );
+		public function __construct() {
+			add_action( 'woocommerce_order_status_changed', array( $this, 'sending_email_on_shipping' ), 10, 4 );
+			add_action( 'woocommerce_email_order_details', array( $this, 'email_customizer' ) );
 
 		}
 
 		/**
 		 * Sending email to Client and admin on Shipment order status.
-		 * 
-		 * @param int $order_id ID of Order.
-		 * @param array $old_status previous status name.
-		 * @param array $new_status current status name.
+		 *
+		 * @param int   $order_id ID of Order.
+		 * @param string $old_status previous status name.
+		 * @param string $new_status current status name.
 		 * @param array $order whole order details.
 		 */
-		public function sending_email_on_shipping($order_id, $old_status, $new_status, $order ) 
-		{ 
+		public function sending_email_on_shipping( $order_id, $old_status, $new_status, $order ) {
 			$mailer = WC()->mailer()->get_emails();
-			if($new_status === 'shipping' ){
-				
+			if ( 'shipping' === $new_status ) {
+
 				$heading = sprintf( 'Your order #%s is now on Shipment', $order->get_id() );
 				$subject = 'Order\'s status changed';
-				$mailer['WC_Email_Customer_Completed_Order']->heading = $heading;
+				$mailer['WC_Email_Customer_Completed_Order']->heading             = $heading;
 				$mailer['WC_Email_Customer_Completed_Order']->settings['heading'] = $heading;
-				$mailer['WC_Email_Customer_Completed_Order']->subject = $subject;
+				$mailer['WC_Email_Customer_Completed_Order']->subject             = $subject;
 				$mailer['WC_Email_Customer_Completed_Order']->settings['subject'] = $subject;
-				$mailer['WC_Email_Customer_Completed_Order']->trigger( $order_id);
+				$mailer['WC_Email_Customer_Completed_Order']->trigger( $order_id );
 
-				//Sending custom email to admin.
-				$admin_mail = get_option('admin_email');
-				$subject = 'Order\'s status changed';
-				$body = sprintf( 'Your order #%s has proceeded to Shipment', $order->get_id() ) ;
-				wp_mail( $admin_mail, $subject, $body, "" );
+				// Sending custom email to admin.
+				$admin_mail = get_option( 'admin_email' );
+				$subject    = 'Order\'s status changed';
+				$body       = sprintf( 'Your order #%s has proceeded to Shipment', $order->get_id() );
+				wp_mail( $admin_mail, $subject, $body, '' );
 			}
 		}
 
 		/**
-		 * client's shipment email customizer.
-		 * @param array $order All orders
+		 * Client's shipment email customizer.
+		 *
+		 * @param array $order All orders.
 		 */
-		public function email_customizer( $order ){
-			$message = '<h3>';
-			$message .= sprintf( 'Order #%s is now proceed to Shipment', $order->get_id() );;
+		public function email_customizer( $order ) {
+			$message  = '<h3>';
+			$message .= sprintf( 'Order #%s is now proceed to Shipment', $order->get_id() );
+
 			$message .= '</h3>';
 			echo $message;
 		}
 	}
 }
 new ES_Sendingemail();
-?>
+
